ddd070f8172eb63ab8b802e58af9266c37be86a6
Robert <rbbenett0614@gmail.com>
2020-12-02 20:32:52 +0000

    Fifth Commit

---
+++ input.js
@@ -0,0 +1,16 @@
+const { builtinModules } = require("module");
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  const handleUserInput = data => {
+    if (key === '\u0003')
+    process.exit();
+  };
+  return stdin;
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,21 +1,7 @@
-const { connect } = require('./client')
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  const handleUserInput = data => {
-    if (key === '\u0003')
-    process.exit();
-  };
-  return stdin;
-};
-
+setupInput;
